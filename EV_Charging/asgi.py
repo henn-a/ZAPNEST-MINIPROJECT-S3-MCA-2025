f8b7d3c9c958c50c19902bb40cd0688ffa305607
henn-a <147914416+henn-a@users.noreply.github.com>
2025-10-02 19:02:29 +0530

    Add files via upload

---
+++ EV_Charging/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for EV_Charging project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EV_Charging.settings')
+
+application = get_asgi_application()
